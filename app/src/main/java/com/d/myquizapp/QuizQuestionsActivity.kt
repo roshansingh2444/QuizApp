ceb176ceb69096e540944bcd2a789d3db2c92d65
roshansingh2444 <48400264+roshansingh2444@users.noreply.github.com>
2022-12-06 13:13:52 +0530

    added 2 screen

---
+++ app/src/main/java/com/d/myquizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,54 @@
+package com.d.myquizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+
+class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+    private var tvOptionOne:TextView? = null
+    private var tvOptionTwo:TextView? = null
+    private var tvOptionThree:TextView? = null
+    private var tvOptionFour:TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+        Log.e("Question Size","${questionsList.size}")
+        for (i in questionsList){
+            Log.e("Questions",i.question)
+        }
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition - 1]
+
+        progressBar=findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
+        progressBar?.progress=
+            currentPosition
+        tvProgress?.text = "$currentPosition" + "/" +progressBar?.max
+
+        tvQuestion?.text = question !! .question
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
+    }
+}
