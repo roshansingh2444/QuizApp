eb3f9d03fdc624f04d1fa3dedb6945e2bf4a6a33
roshansingh2444 <48400264+roshansingh2444@users.noreply.github.com>
2023-01-04 17:38:32 +0530

    finished with result page

--- app/src/main/java/com/d/myquizapp/ResultActivity.kt
+++ app/src/main/java/com/d/myquizapp/ResultActivity.kt
@@ -3,6 +3,8 @@ package com.d.myquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// TODO (STEP 3: Create a result activity for showing the score.)
+// START
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
